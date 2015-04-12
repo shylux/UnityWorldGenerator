f03d3092212cf700005864e20d0aad78ae1628c5
Lukas Knoepfel <shylux@gmail.com>
2015-04-12 14:05:04 +0200

    Added countdown and lobby.

---
+++ Assets/StartCountdown.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Vehicles.Car;
+
+public class StartCountdown : MonoBehaviour {
+	public int CountMax = 3;
+	private int _countDown;
+
+	private CarController CarScript;
+
+	// Use this for initialization
+	void Start () {
+		CarScript = GetComponent<CarController> ();
+		print ("Begin Start" + Time.time);
+		StartCoroutine (GameStart ());
+		print ("End Start" + Time.time);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator GameStart() {
+		CarScript.enabled = false;
+		for (_countDown = CountMax; _countDown > 0; _countDown--) {
+			yield return new WaitForSeconds(1);
+			print("WaitForSeconds"+Time.time);
+		}
+		CarScript.enabled = true;
+	}
+}
