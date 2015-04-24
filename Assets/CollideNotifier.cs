4a02c130104efa2cbbe2b9210a08c31977435f50
Lukas Knoepfel <shylux@gmail.com>
2015-04-24 16:26:07 +0200

    Added finish event.

---
+++ Assets/CollideNotifier.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class CollideNotifier : MonoBehaviour {
+	public class CollideEvent : UnityEvent<Collider> {};
+
+	public CollideEvent onCollide = new CollideEvent();
+
+	void OnTriggerEnter(Collider c) {
+		onCollide.Invoke (c);
+	}
+}
--- Assets/MazeRunner.cs
+++ Assets/MazeRunner.cs
@@ -1,17 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 using System.Collections;
 
 public class MazeRunner : MonoBehaviour {
     public Transform Car;
 
+	float startTime;
+
 	// Use this for initialization
 	void Start () {
 	    Car.position = transform.Find("Start").position;
 
+		startTime = Time.time;
+
+		// Register event
+	    GameObject finish = GameObject.FindGameObjectsWithTag ("Finish")[0];
+		CollideNotifier notif = finish.GetComponent<CollideNotifier> ();
+		notif.onCollide.AddListener (runnerFinished);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void runnerFinished(Collider c) {
+		Debug.Log ("Time: "+(Time.time - startTime));
 	}
 }
